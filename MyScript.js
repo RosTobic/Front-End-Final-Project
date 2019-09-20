a34d9003094ccca4817311ff772326196b9de2a5
RosTobic <54326493+RosTobic@users.noreply.github.com>
2019-09-20 10:01:06 -0700

    work

--- MyScript.js
+++ MyScript.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$(".img").click(function(){ 
 	$(".img1").text(x = Math.floor(Math.random() * 11));
+	$("").html(x = Math.floor(Math.random() * 11));
 	});
 	$(".text").click(function(){ 
 	$(".text1").text(x = Math.floor(Math.random() * 11));
