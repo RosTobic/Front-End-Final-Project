94d88ed65f6c880b725b53a1440e15d75cefce85
RosTobic <54326493+RosTobic@users.noreply.github.com>
2019-09-23 09:53:46 -0700

    added random

--- MyScript.js
+++ MyScript.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$(".img").click(function(){ 
 	$(".img1").text(x = Math.floor(Math.random() * 11));
-	$("").html(x = Math.floor(Math.random() * 11));
 	});
 	$(".text").click(function(){ 
 	$(".text1").text(x = Math.floor(Math.random() * 11));
