2741588f1bda78cd0fe5c946d8fbf89616a1390b
RosTobic <54326493+RosTobic@users.noreply.github.com>
2019-09-24 10:00:14 -0700

    added random

--- MyScript.js
+++ MyScript.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
-	$(".img").click(function(){ 
-	$(".img1").text(x = Math.floor(Math.random() * 11));
-	});
 	$(".text").click(function(){ 
 	$(".text1").text(x = Math.floor(Math.random() * 11));
+	});
+	$(".img").click(function(){
+	$("#theImage").attr("src", MemeSwitch[
+		Math.round(Math.random()*11)])
+	
+	});
+	
+	
+	$(".img").click(function(){
+	$("#memeText").text(MemeCaptions[
+		Math.round(Math.random()*11)])
 	});
 	});
